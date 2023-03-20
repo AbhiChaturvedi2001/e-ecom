a8c25641473ad5313c15b2f3966831c130ddda15
ABHCH <chaturvedia435@gmail.com>
2023-03-20 09:39:15 +0530

    Update UI

---
+++ src/FrontendStack.js
@@ -0,0 +1,26 @@
+const FrontEnd = [
+    {
+        id:1,
+        progLang:'Javascript',
+    },
+    {
+        id:1,
+        progLang:'React',
+    },
+    {
+        id:1,
+        progLang:'Redux',
+    },
+    {
+        id:1,
+        progLang:'Tailwind',
+    },
+]
+
+export default FrontEnd;
+
+const others = [
+    {
+        
+    },
+]
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,14 +1,50 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React, { useEffect } from "react";
+import Navbar from "../Components/Navbar";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const About = ({Toggle , Mode, showNavbar, changeMode}) => {
+const About = ({ Toggle, Mode, showNavbar, changeMode }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
-    <div className='about-page-container'>
-      <Navbar Toggle={Toggle} Mode={Mode} showNavbar={showNavbar} changeMode={changeMode} />
-    </div>
+      <div className="about-page-container">
+        <Navbar
+          Toggle={Toggle}
+          Mode={Mode}
+          showNavbar={showNavbar}
+          changeMode={changeMode}
+        />
+        <div className='relative text-center data-aos="fade-right"'>
+          <h1
+            data-aos="fade-right"
+            className="heading font-concert left-[35%] -z-4  "
+          >{`<About />`}</h1>
+        </div>
+        <div className="mt-6 m-auto md:mt-6 md:w-[80%] w-[51%]">
+          <div className="text-3xl font-concert">🤓 About Me</div>
+          <div className="w-[100%] font-poppins font-medium">
+            I am a Full-Stack creative developer based in Barielly, India. I am
+            a Computer Science undergraduate from Chandigarh University and also
+            did some coursework in Designing and Visual Arts. I enjoy using my
+            skill-set to empower people to accomplish their goals.<br/> <br /> My
+            development stack is focused on converting ideas into sites & apps
+            that work. I create lighting fast WebApps and Websites using MERN
+            Stack with the help of NextJS.
+          </div>
+        <div className="mt-4 font-concert">Want to know More About Me ? </div>
+        <button className="px-2 py-1 mt-3 bg-orange-400 text-white ">Know More</button>
+        </div>
+      </div>
+      <div className="tech-stack-container relative text-center">
+      <h1
+            data-aos="fade-right"
+            className="heading font-concert left-[35%] -z-4 "
+          >{`< weapons />`}</h1>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
